5806e1678f581a48431b4f387adfed8fa51a51a0
4ndr31 <andrei.pinto@escola.pr.gov.br>
2025-05-17 17:29:40 -0300

    ultima versão até o carrossel

--- js/index.js
+++ js/index.js
@@ -59,3 +59,35 @@ window.addEventListener('resize', () => {
     document.body.style.overflow = '';
   }
 });
+
+
+
+// swiper
+new Swiper('.card-wrapper', {
+  // Optional parameters
+  loop: true,
+  spaceBetween: 30,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints:{
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    },
+  }
+});
